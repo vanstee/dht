6cfe6295e8d7e48d5b9503797854baf66158782f
Patrick Van Stee <pvanste@clemson.edu>
2010-08-28 15:17:10 -0400

    Refactor methods while keeping the overall structure of the api.

--- server.py
+++ server.py
@@ -1,159 +1,67 @@
-from bottle  import route, request, run, app
-from urllib2 import urlopen
-from hashlib import sha1
-from sys     import argv
-from socket  import gethostname, gethostbyname
-	
-@route('/set/:key/:value')
-def setroute(key, value):
-	node = findnode(key)
-	keyhash = int(sha1(key).hexdigest(), 16)
-	if node == address:
-		try:
-			data[keyhash] = value
-			print 'local key set success'
-			return 'success'
-		except:
-			print 'local key set failure'
-			return 'failure'
-	else:
-		try:
-			url = urlopen('http://%s/set/%s/%s' % (node, key, value))
-			response = url.read()
-			url.close()
-			print 'remote key set success'
-			return response
-		except:
-			print 'remote key set failure'		
-			return 'failure'
+from bottle   import run, route, app, request, redirect
+from urllib2  import urlopen
+from urlparse import urlparse
+from hashlib  import sha1
+from sys      import argv
+from socket   import gethostname, gethostbyname
 
-@route('/get/:key')
-def getroute(key):
-	node = findnode(key)
-	keyhash = int(sha1(key).hexdigest(), 16)
-	if node == address:
-		try:
-			if keyhash in data:			
-				print 'local key get success'
-				return data[keyhash]
-			else:
-				print 'local key get failure (missing key)'
-				return 'failure'
-		except:
-			print 'local key get failure'
-			return 'failure'
-	else:
-		try:
-			url = urlopen('http://%s/get/%s' % (node, key))
-			response = url.read()
-			url.close()
-			print 'remote key get success'
-			return response
-		except:
-			print 'remote key get failure'
-			return 'failure'
-		
-@route('/exists/:key')
-def existsroute(key):
-	node = findnode(key)
-	keyhash = int(sha1(key).hexdigest(), 16)
-	if node == address:
-		try:
-			if keyhash in data:
-				print 'local key exists success'
-				return 'success'
-			else:
-				print 'local key exists failure (missing key)'
-				return 'failure'
-		except:
-			print 'local key exists failure'
-			return 'failure'
-	else:
-		try:
-			url = urlopen('http://%s/get/%s' % (node, key))
-			response = url.read()
-			url.close()
-			print 'remote key exists success'
-			return response
-		except:
-			print 'remote key exists failure'
-			return 'failure'
-		
-@route('/nodes')
-def nodesroute():
-	if nodes:
-		return ' '.join(nodes)
-	else:
-		return ''
-		
-@route('/size')
-def sizeroute():
-	return str(partition[1] - partition[0])
-	
-@route('/split')
-def splitroute():
-	start = partition[0]
-	end   = partition[1]
-	middle = ((end - start) / 2) + start
-	partition[1] = middle
-	return '%d %d' % (middle + 1, end)
-	
-@route('/contains/:key')
-def containsroute(key):
-	keyhash = int(sha1(key).hexdigest(), 16)
-	start = partition[0]
-	end   = partition[1]	
-	if start >= keyhash or end <= keyhash:
-		return 'true'
-	else:
-		return 'false'
-		
-def findnode(key):
-	for node in nodes:
-		try:
-			url = urlopen('http://%s/contains/%s' % (node, key))
-			response = url.read()
-			print '%s %s' % (node, response)
-			url.close()
-			if bool(response):
-				return node
-		except:
-			pass
-	raise Exception('missing node')
-	return
-
-address = gethostbyname(gethostname())
-data = {}
-nodes = []
-partition = [0x0L, 0xffffffffffffffffffffffffffffffffffffffffL] # sha1 space
+data     = []
+keyspace = [0, 128]
+nodes    = set()
+host     = gethostbyname(gethostname())
 
 if len(argv) == 2:
-	node = argv[1]
+	join()
 
-	print 'http://%s/nodes' % node
+def join():
+	global nodes
+	node = argv[1]
 	url = urlopen('http://%s/nodes' % node)
 	response = url.read()
 	url.close()
 	if response:
-		nodes = response.split(' ')
-	nodes.append(node)
-
-	max_node = nodes[0]
-	max_size = sizeroute()
+		nodes = set(response.split(' '))
+	nodes.add(node)
+	
+	max_node = node
+	url = urlopen('http://%s/size' % node)
+	max_size = int(url.read())
+	url.close()
 	for node in nodes:
-		print 'http://%s/size' % node
 		url = urlopen('http://%s/size' % node)
-		node_size = url.read()
+		node_size = int(url.read())
 		url.close()
-		if max_node > max_size:
+		if node_size > max_size:
 			max_node = node
 			max_size = node_size
 	node = max_node
+	
+	url = urlopen('http://%s/split')
+	keyspace = url.read().split(' ')
+	url.close()
+		
+@route('/set/:key/value')
+def setroute(key, value):
+	pass
 
-	url = urlopen('http://%s/split' % node)
-	partition = url.read().split(' ')
-	print partition
-	url.close()	
+@route('/get/:key')
+def getroute(key):
+	pass
+
+@route('/nodes')
+def nodesroute():
+	return ' '.join(nodes)
+
+@route('/size')
+def sizeroute():
+	nodes.add(urlparse(request.url).hostname)
+	return keyspace[1] - keyspace[0]
+
+@route('/split')
+def splitroute():
+	start, end = keyspace
+	keyspace[1] = int((start + end) / 2)
+	return '%d %d' % (keyspace[1] + 1, end)
 
 app().catchall = False
-run(host=address, port=8080)
+run(host = host, port = 8080)
